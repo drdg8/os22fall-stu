5c4627c0267025647cc6c980b40dd8de3bb3fb7e
Xingkai Wang <bittervanzju@gmail.com>
2023-01-11 11:39:26 +0800

    Update getpid.c

--- src/lab6/user/getpid.c
+++ src/lab6/user/getpid.c
@@ -31,7 +31,7 @@ int global_variable = 0;
 //   pid = fork();
 
 //     while(1) {
-//         printf("[PID = %d] is running, variable: %d\n", getpid(), variable++);
+//         printf("[PID = %d] is running, variable: %d\n", getpid(), global_variable++);
 //         for (unsigned int i = 0; i < 0x7FFFFFF; i++);
 //     }
 // }
